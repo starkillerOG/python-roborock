7507423478c0a35375cc51fbffa043f015d73755
Allen Porter <allen.porter@gmail.com>
2025-07-14 19:28:33 -0700

    chore: Create module for v1 request encoding (#413)
    * chore: Create module for v1 request encoding
    
    * chore: Delete tests/devices/test_v1_protocol.py
    
    * feat: Simplify local payload encoding by rejecting any cloud commands sent locally

--- tests/test_local_api_v1.py
+++ tests/test_local_api_v1.py
@@ -86,7 +86,7 @@ async def test_get_room_mapping(
     )
     response_queue.put(message)
 
-    with patch("roborock.version_1_apis.roborock_client_v1.get_next_int", return_value=test_request_id):
+    with patch("roborock.protocols.v1_protocol.get_next_int", return_value=test_request_id):
         room_mapping = await connected_local_client.get_room_mapping()
 
     assert room_mapping == [
